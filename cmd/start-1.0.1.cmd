bf1c55fa28667299c12fbb56a5b4f21d95938cf7
Samuel Johns <121411608+samcjohns@users.noreply.github.com>
2024-11-30 13:31:29 -0500

    Debugging

--- cmd/start-1.0.1.cmd
+++ cmd/start-1.0.1.cmd
@@ -5,4 +5,4 @@ REM Change the registry key for the default window size
 reg add "HKEY_CURRENT_USER\Console" /v WindowSize /t REG_BINARY /d 78002400 /f
 
 REM Launch Java in the current CMD window
-"%~dp0runtime\bin\java.exe" -jar "%~dp0tetrad-1.0.0.jar" -PROD
+"%~dp0runtime\bin\java.exe" -jar "%~dp0tetrad-1.0.1.jar" -PROD
